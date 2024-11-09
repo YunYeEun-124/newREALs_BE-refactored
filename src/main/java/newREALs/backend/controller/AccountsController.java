6892b237ad543c1182c3e832680c0ec4f985f8f7
Seohyun <tjgus9138@naver.com>
2024-11-09 15:39:07 +0900

    [feat] 로그인 구현 다시

--- src/main/java/newREALs/backend/controller/AccountsController.java
+++ src/main/java/newREALs/backend/controller/AccountsController.java
@@ -1,14 +1,47 @@
 package newREALs.backend.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import newREALs.backend.service.KakaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
 @RequestMapping("/accounts")
+@RequiredArgsConstructor
 public class AccountsController {
-    @GetMapping
-    public String loginWithKakao() {
-        return "redirect:/oauth2/authorization/kakao";
+
+    private final KakaoService kakaoService;
+    private final Gson gson = new Gson();
+
+    @PostMapping("/login")
+    public ResponseEntity<String> kakaoLogin(@RequestBody Map<String, String> request) {
+        String authorizationCode = request.get("code");
+        Map<String, Object> kakaoResponse = kakaoService.processKakaoLogin(authorizationCode);
+
+        String redirectUrl;
+        if ((boolean) kakaoResponse.get("isNewAccount")) {
+            redirectUrl = "/register";
+        } else {
+            redirectUrl = "/home";
+        }
+
+        // 프론트에 전달할 내용
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("status", "success");
+        responseBody.put("access_token", kakaoResponse.get("accessToken"));
+        responseBody.put("redirect_url", redirectUrl);
+        responseBody.put("name", kakaoResponse.get("name"));
+        responseBody.put("email", kakaoResponse.get("email"));
+        responseBody.put("user_pk", kakaoResponse.get("userPk"));
+
+        // JSON 응답으로
+        String jsonResponse = gson.toJson(responseBody);
+
+        return new ResponseEntity<>(jsonResponse, HttpStatus.OK);
     }
 }
