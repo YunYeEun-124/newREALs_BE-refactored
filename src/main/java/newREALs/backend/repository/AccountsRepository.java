0c2268c899a15c7c65c4017265a46d49faf3a865
Seohyun <tjgus9138@naver.com>
2024-11-11 12:53:26 +0900

    [feat] AttendanceList 매달 초기화

--- src/main/java/newREALs/backend/repository/AccountsRepository.java
+++ src/main/java/newREALs/backend/repository/AccountsRepository.java
@@ -3,7 +3,6 @@ package newREALs.backend.repository;
 import newREALs.backend.domain.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface AccountsRepository extends JpaRepository<Accounts, Long> {
