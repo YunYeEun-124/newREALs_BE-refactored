4e9f5d151ffb906189ebff4ce306d9694bf0422d
Seohyun <tjgus9138@naver.com>
2024-11-11 13:02:20 +0900

    [update] UserRepository 이름 변경

--- src/main/java/newREALs/backend/repository/UserRepository.java
+++ src/main/java/newREALs/backend/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface AccountsRepository extends JpaRepository<Accounts, Long> {
+public interface UserRepository extends JpaRepository<Accounts, Long> {
     Optional<Accounts> findByEmail(String email); // email로 Accounts 조회
 }
