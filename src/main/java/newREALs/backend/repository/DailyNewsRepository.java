180d678ba673628e6fcbb61e2ded058a8202e987
Seohyun <tjgus9138@naver.com>
2024-11-13 11:14:07 +0900

    [create] 프로필 정보 조회

--- src/main/java/newREALs/backend/repository/DailyNewsRepository.java
+++ src/main/java/newREALs/backend/repository/DailyNewsRepository.java
@@ -4,9 +4,7 @@ import newREALs.backend.domain.Dailynews;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface DailyNewsRepository extends JpaRepository<Dailynews,Long> {
-    
+    Dailynews findByUserId(Long userId);
 }
