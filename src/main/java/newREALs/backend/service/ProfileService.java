e4b68a88185c71082f15f5dabafeea9f40fd2a2c
Seohyun <tjgus9138@naver.com>
2024-11-14 17:20:23 +0900

    [update] 스크랩 뉴스 개수 변경

--- src/main/java/newREALs/backend/service/ProfileService.java
+++ src/main/java/newREALs/backend/service/ProfileService.java
@@ -77,7 +77,7 @@ public class ProfileService {
     public Pageable getPageInfo(int page) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("uploadDate"));
-        return PageRequest.of(page - 1, 8, Sort.by(sorts));
+        return PageRequest.of(page - 1, 9, Sort.by(sorts));
     }
 
     public Page<BaseNewsThumbnailDTO> getScrapNewsThumbnail(Long userId, int page) {
@@ -101,5 +101,92 @@ public class ProfileService {
                 .isScrap(true)
                 .build()
         );
+    }
+
+    public Map<String, List<ProfileInterestDTO>> getInterest(Long userId) {
+        Accounts account = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("없는 userId"));
+
+        Map<String, List<ProfileInterestDTO>> result = new HashMap<>();
+
+        Pageable three = PageRequest.of(0,3);
+
+        result.put("total", new ArrayList<>());
+        result.put("politics", new ArrayList<>());
+        result.put("economy", new ArrayList<>());
+        result.put("society", new ArrayList<>());
+
+        // 카테고리 상관 없이 전체에서 3개 가져오기
+        List<Object[]> totalInterest = accountsRepository.findTotalInterestByUserId(userId, three);
+        List<ProfileInterestDTO> totalInterestDTO = getTotalPercentage(totalInterest);
+        result.put("total", totalInterestDTO); // key를 total로
+
+        // 카테고리 별로 3개 가져오기
+        List<Object[]> categoryInterest = accountsRepository.findCategoryInterestByUserId(userId, three);
+        Map<String, List<ProfileInterestDTO>> categoryInterestDTO = getCategoryPercentage(categoryInterest);
+        result.putAll(categoryInterestDTO);
+
+
+        return result;
+    }
+
+    private List<ProfileInterestDTO> getTotalPercentage(List<Object[]> interests) {
+        List<ProfileInterestDTO> interestDTOList = new ArrayList<>();
+
+        int total = 0;
+        for (Object[] item : interests) {
+            total += (int) item[2];
+        }
+
+        for (Object[] item : interests) {
+            String category = (String) item[0];
+            String subCategory = (String) item[1];
+            int count = (int) item[2];
+
+            int percentage = (int) Math.round((count * 100.0) / total);
+            ProfileInterestDTO dto = ProfileInterestDTO.builder()
+                    .category(category)
+                    .subCategory(subCategory)
+                    .percentage(percentage)
+                    .build();
+            interestDTOList.add(dto);
+        }
+        return interestDTOList;
+    }
+
+    private Map<String, List<ProfileInterestDTO>> getCategoryPercentage(List<Object[]> interests) {
+        Map<String, List<ProfileInterestDTO>> result = new HashMap<>();
+        Map<String, Integer> categoryTotals = new HashMap<>(); // 카테고리 별로 count 합 저장
+
+        for (Object[] item : interests) {
+            String category = (String) item[0];
+            int count = (int) item[2];
+            categoryTotals.put(category, categoryTotals.getOrDefault(category, 0) + count); // 기본값은 0으로
+        }
+
+        // 카테고리별 퍼센트 계산
+        for (Object[] item : interests) {
+            String category = (String) item[0];
+            String subCategory = (String) item[1];
+            int count = (int) item[2];
+            int categoryTotal = categoryTotals.get(category);
+
+            int percentage = 0;
+            for (int j = 0; j < count; j++) {
+                percentage = (int) Math.round((count * 100.0) / categoryTotal);
+            }
+
+            ProfileInterestDTO dto = ProfileInterestDTO.builder()
+                    .category(category)
+                    .subCategory(subCategory)
+                    .percentage(percentage)
+                    .build();
+
+            if (!result.containsKey(category)) {
+                result.put(category, new ArrayList<>());
+            }
+            result.get(category).add(dto);
+        }
+        return result;
     }
 }
