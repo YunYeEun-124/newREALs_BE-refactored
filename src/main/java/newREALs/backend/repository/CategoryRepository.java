a95a4a4bf96d6dfd155167bb7fd99f4fda9e43cf
yeeun <drrrkak@gmail.com>
2024-11-18 18:37:25 +0900

    [update] Basenews domain

--- src/main/java/newREALs/backend/repository/CategoryRepository.java
+++ src/main/java/newREALs/backend/repository/CategoryRepository.java
@@ -1,6 +1,6 @@
 package newREALs.backend.repository;
 
-import kotlin.ReplaceWith;
+
 import newREALs.backend.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
