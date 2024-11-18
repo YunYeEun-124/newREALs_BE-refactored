7854d909355a59cdd2f17ea04344c04ec22852ac
naaadang <naaadang@naver.com>
2024-11-18 18:19:19 +0900

    Merge CategoryNewsController, MainNewsController, SearchController

---
+++ src/main/java/newREALs/backend/controller/MainNewsController.java
@@ -0,0 +1,65 @@
+package newREALs.backend.controller;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import newREALs.backend.domain.UserKeyword;
+import newREALs.backend.dto.DailyNewsThumbnailDTO;
+import newREALs.backend.dto.KeywordNewsDTO;
+import newREALs.backend.service.NewsService;
+import newREALs.backend.service.NewsService2;
+import newREALs.backend.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("main/")
+public class MainNewsController {
+
+
+    private final NewsService2 newsService;
+    private final TokenService tokenService;
+
+    public MainNewsController(NewsService2 newsService, TokenService tokenService) {
+        this.newsService = newsService;
+        this.tokenService = tokenService;
+    }
+
+    //main news list
+    @GetMapping("/daily")
+    public ResponseEntity<?> viewDailynewsList(HttpServletRequest userInfo){
+
+        List<DailyNewsThumbnailDTO> result = newsService.getDailynewsList();
+        if(result.size() != 5){ //error
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("timestamp", LocalDateTime.now());
+            errorResponse.put("status", 400);
+            errorResponse.put("error", "Dailynews List is not ready");
+            errorResponse.put("path", "/main/daily");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+
+        }else  return  ResponseEntity.ok().body(result);
+
+
+
+    }
+    //think
+
+    //keyword newsList, 사용자 키워드 리스트
+    @GetMapping("/keyword")
+    public ResponseEntity<?> viewKeywordnewsList(HttpServletRequest userInfo, @RequestParam int keywordIndex,@RequestParam int page){
+        Long userid = tokenService.getUserId(userInfo);
+        KeywordNewsDTO keywordnewsList =  newsService.getKeywordnewsList(userid,keywordIndex,page);
+
+        return ResponseEntity.ok().body(keywordnewsList);
+    }
+
+}
---
+++ src/main/java/newREALs/backend/controller/SearchController.java
@@ -0,0 +1,46 @@
+package newREALs.backend.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import newREALs.backend.dto.SearchDTO;
+import newREALs.backend.service.NewsService;
+import newREALs.backend.service.NewsService2;
+import newREALs.backend.service.TokenService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/search")
+public class SearchController {
+
+    private final NewsService2 newsService;
+    private final TokenService tokenService;
+
+    public SearchController(NewsService2 newsService, TokenService tokenService) {
+        this.newsService = newsService;
+        this.tokenService = tokenService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getNewsList(HttpServletRequest userInfo, @RequestParam String searchWord, @RequestParam int page){
+
+        Long userid = tokenService.getUserId(userInfo);
+        //./ Long userid = 1l;
+        if(searchWord.isEmpty()){
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("timestamp", LocalDateTime.now());
+            errorResponse.put("status", 400);
+            errorResponse.put("error", "searchWord is empty");
+            errorResponse.put("path", "/search");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+        }
+
+        SearchDTO result = newsService.getSearch(userid,searchWord,page);
+
+        return  ResponseEntity.ok().body(result);
+    }
+}
