dad6371388bfe2fad79d4a752a542965f9072b42
naaadang <126650849+naaadang@users.noreply.github.com>
2024-11-21 12:43:18 +0900

    [Update] GetNaverNews.java

--- src/main/java/newREALs/backend/service/externalAPI/GetNaverNews.java
+++ src/main/java/newREALs/backend/service/externalAPI/GetNaverNews.java
@@ -65,7 +65,7 @@ public class GetNaverNews {
     }
 
 
-    @Scheduled(cron = "0 40 12 ? * *")
+    @Scheduled(cron = "0 50 12 ? * *")
     @Transactional
     public void getBasenews() {
         List<Keyword> keywords = keywordRepository.findAll(); //key word 다 불러와
@@ -83,7 +83,7 @@ public class GetNaverNews {
 
 
     //매일 아침마다 하루 한 번 실행
-    @Scheduled(cron = "0 40 12 ? * *")
+    @Scheduled(cron = "0 50 12 ? * *")
     @Transactional
     public void getDailynews(){
 
