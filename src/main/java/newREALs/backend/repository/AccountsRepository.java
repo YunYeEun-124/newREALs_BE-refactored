b4d2a4eaec556062ed297fb479ad2d53e0de15c6
Seohyun <tjgus9138@naver.com>
2024-11-22 05:36:31 +0900

    [update] 관심도 분석 오류 해결

--- src/main/java/newREALs/backend/repository/AccountsRepository.java
+++ src/main/java/newREALs/backend/repository/AccountsRepository.java
@@ -2,6 +2,7 @@ package newREALs.backend.repository;
 
 import newREALs.backend.domain.Accounts;
 import newREALs.backend.domain.Basenews;
+import newREALs.backend.dto.ProfileInterestProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -18,20 +19,27 @@ public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 
     // LIMIT 사용 안됨 -> Pageable로 가져오는 개수 정하기
     // 카테고리별 관심도 count 상위 3개
-    @Query("SELECT sc.category.name, sc.name, si.count " +
-            "FROM SubInterest si " +
-            "JOIN si.subCategory sc " +
-            "WHERE si.user.id = :userId AND sc.category.name = :category " +
-            "ORDER BY si.count DESC")
-    List<Object[]> findCategoryInterestById(Long userId, String category, Pageable pageable);
+    @Query(value = "SELECT c.name AS category, sc.name AS subCategory, SUM(si.count) AS count " +
+            "FROM sub_interest si " +
+            "JOIN sub_category sc ON si.sub_category_id = sc.id " +
+            "JOIN category c ON sc.category_id = c.id " +
+            "WHERE si.user_id = :userId AND c.name = :category " +
+            "GROUP BY c.name, sc.name " +
+            "ORDER BY SUM(si.count) DESC " +
+            "LIMIT 3", nativeQuery = true)
+    List<ProfileInterestProjection> findCategoryInterestById(Long userId, String category);
 
 
     // 전체 관심도 count 상위 3개
-    @Query("SELECT si.subCategory.category.name, si.subCategory.name, si.count " +
-            "FROM SubInterest si " +
-            "WHERE si.user.id = :userId " +
-            "ORDER BY si.count DESC")
-    List<Object[]> findTotalInterestById(Long userId, Pageable pageable);
+    @Query(value = "SELECT c.name AS category, sc.name AS subCategory, SUM(si.count) AS count " +
+            "FROM sub_interest si " +
+            "JOIN sub_category sc ON si.sub_category_id = sc.id " +
+            "JOIN category c ON sc.category_id = c.id " +
+            "WHERE si.user_id = :userId " +
+            "GROUP BY c.name, sc.name " +
+            "ORDER BY SUM(si.count) DESC " +
+            "LIMIT 3", nativeQuery = true)
+    List<ProfileInterestProjection> findTotalInterestById(Long userId);
 
     // 프로필 정보 수정
     @Query("SELECT a.name, a.profilePath " +
