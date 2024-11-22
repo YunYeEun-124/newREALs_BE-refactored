931abd840bfdc9bee8f389a42943a5cfc2964249
yeeun <drrrkak@gmail.com>
2024-11-23 04:39:50 +0900

    API response update

--- src/main/java/newREALs/backend/controller/CategoryNewsController.java
+++ src/main/java/newREALs/backend/controller/CategoryNewsController.java
@@ -3,6 +3,7 @@ package newREALs.backend.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import newREALs.backend.domain.SubCategory;
+import newREALs.backend.dto.ApiResponseDTO;
 import newREALs.backend.dto.ViewCategoryDTO;
 import newREALs.backend.repository.SubCategoryRepository;
 import newREALs.backend.service.NewsService;
@@ -35,31 +36,30 @@ public class CategoryNewsController {
     public ResponseEntity<?> viewSubCategory(HttpServletRequest userInfo, @RequestParam String category, @RequestParam String subCategory,@RequestParam int page){
         Long userid = tokenService.getUserId(userInfo);
         // 카테고리 입력 확인
-        if (category.isEmpty()||subCategory.isEmpty()) {
-            return createResponse(false, "E400", "카테고리를 입력해주세요", null, HttpStatus.BAD_REQUEST);
-        }
-        if(!category.equals("society") && !category.equals("politics") && !category.equals("economy") ){
-            return createResponse(false, "E400", "잘못된 카테고리 입력입니다.", category, HttpStatus.BAD_REQUEST);
-        }
+        if (category.isEmpty()||subCategory.isEmpty())
+             throw new IllegalArgumentException("매개변수 null");
+
+        if(!category.equals("society") && !category.equals("politics") && !category.equals("economy") )
+            throw new IllegalArgumentException("잘못된 카테고리 입력.");
+
 
         Optional<SubCategory> sub = subCategoryRepository.findByName(subCategory);
 
-        if (sub.isPresent()&& category.equals(sub.get().getCategory().getName())){ //subcategory - category mapping check
+        if (sub.isPresent() && category.equals(sub.get().getCategory().getName())){ //subcategory - category mapping check
              ViewCategoryDTO result  = newsService.getSubCategory(userid,category,subCategory,page);
             // 결과가 비어 있는 경우
-            if (result.getBasenewsList().isEmpty() || result.getDailynews() == null) {
-                return createResponse(false, "E500", "뉴스 불러오기 실패", null, HttpStatus.NOT_FOUND);
-            }
-
+            if (result.getBasenewsList().isEmpty() || result.getDailynews() == null)
+               throw new IllegalStateException("뉴스 불러오기 실패");
             // 페이지 범위 확인
-            if (page > result.getTotalPage()) {
-                return createResponse(false, "E400", "Page index 초과 또는 유효하지 않은 페이지 요청", null, HttpStatus.BAD_REQUEST);
-            }
+            if (page <= 0 ||page > result.getTotalPage())
+                throw  new IllegalArgumentException("페이지 범위 오류");
+            return ResponseEntity.ok(
+                    ApiResponseDTO.success( "소 카테고리 뉴스 조회 성공.", result)
+            );
 
-            return createResponse(true, "S200", subCategory + " 페이지 조회 성공", result, HttpStatus.OK);
-        }else{
-            return createResponse(false, "E400", " 잘못된 소카테고리 입니다. ",subCategory, HttpStatus.BAD_REQUEST);
-        }
+        }else
+            throw  new IllegalArgumentException("잘못된 소카테고리-카테고리 mapping 입니다.");
+
 
 
     }
@@ -71,36 +71,27 @@ public class CategoryNewsController {
 
         // 카테고리 입력 확인
         if (category == null || category.isEmpty()) {
-            return createResponse(false, "E400", "카테고리를 입력해주세요", null, HttpStatus.BAD_REQUEST);
+            throw  new IllegalArgumentException("매개변수 null");
         }
         if(!category.equals("society") && !category.equals("politics") && !category.equals("economy") ){
-            System.out.println("category is wrong");
-            return createResponse(false, "E400", "잘못된 카테고리 입력입니다.", category, HttpStatus.BAD_REQUEST);
+            throw  new IllegalArgumentException("잘못된 카테고리 입력");
         }
         ViewCategoryDTO result = newsService.getCategory(userid, category, page);
 
         // 결과가 비어 있는 경우
         if (result.getBasenewsList().isEmpty() || result.getDailynews() == null) {
-            return createResponse(false, "E500", "뉴스 불러오기 실패", null, HttpStatus.NOT_FOUND);
+            throw  new IllegalStateException("데이터 불러오기 실패");
         }
         // 페이지 범위 확인
         if (page < 1 || page > result.getTotalPage()) {
-            return createResponse(false, "E400", "Page index 초과 또는 유효하지 않은 페이지 요청", null, HttpStatus.BAD_REQUEST);
+            throw  new IllegalArgumentException("페이지 범위 오류");
         }
-
-
-
         // 성공 응답
-        return createResponse(true, "S200", category + " 페이지 조회 성공", result, HttpStatus.OK);
+        return ResponseEntity.ok(
+                ApiResponseDTO.success( "카테고리 뉴스 조회 성공", result)
+        );
     }
 
-    private ResponseEntity<Map<String, Object>> createResponse(boolean isSuccess, String code, String message, Object data, HttpStatus status) {
-        Map<String, Object> response = new LinkedHashMap<>();
-        response.put("isSuccess", isSuccess);
-        response.put("code", code);
-        response.put("message", message);
-        response.put("data", data);
-        return ResponseEntity.status(status).body(response);
-    }
+
 
 }
