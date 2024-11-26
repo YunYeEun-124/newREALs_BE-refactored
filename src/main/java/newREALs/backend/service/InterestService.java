8169787a87899987e0439623829d30893cb54d3f
naaadang <126650849+naaadang@users.noreply.github.com>
2024-11-26 10:05:40 +0900

    [Update] InterestService.java

--- src/main/java/newREALs/backend/service/InterestService.java
+++ src/main/java/newREALs/backend/service/InterestService.java
@@ -13,13 +13,5 @@ import java.util.List;
 @RequiredArgsConstructor
 public class InterestService {
 
-    private final UserRepository userRepository;
-    private final UserKeywordRepository userKeywordRepository;
-
-    public void recommendNewKeywords(Long userId){
-        Accounts user=userRepository.findById(userId)
-                .orElseThrow(()->new EntityNotFoundException("해당 ID의 사용자를 찾을 수 없습니다."));
-
-    }
 
 }
