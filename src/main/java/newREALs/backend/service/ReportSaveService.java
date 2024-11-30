1865445a6c6c5ab3c9fc151ab4bf1b0f35f11bbc
naaadang <126650849+naaadang@users.noreply.github.com>
2024-11-30 17:01:21 +0900

    [Update] ReportSaveService.java

--- src/main/java/newREALs/backend/service/ReportSaveService.java
+++ src/main/java/newREALs/backend/service/ReportSaveService.java
@@ -26,7 +26,7 @@ public class ReportSaveService {
 
     // 레포트 생성
     @Transactional
-    @Scheduled(cron = "00 00 16 30 * ?")
+    @Scheduled(cron = "00 05 1730 * ?")
     public void makeReports() throws JsonProcessingException {
         List<Accounts> users = userRepository.findAll();
         for(Accounts user : users) {
