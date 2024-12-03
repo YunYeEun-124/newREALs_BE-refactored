b38dba32bebd7bdf6f3df2a19b08e487c57d9710
YunYeEun-124 <142516460+YunYeEun-124@users.noreply.github.com>
2024-12-04 01:28:48 +0900

    Update GetNaverNews.java

--- src/main/java/newREALs/backend/service/externalAPI/GetNaverNews.java
+++ src/main/java/newREALs/backend/service/externalAPI/GetNaverNews.java
@@ -63,10 +63,10 @@ public class GetNaverNews {
         this.keywordProcessingService = keywordProcessingService;
     }
 
-    @Scheduled(cron = "0 35 00 ? * *")
-    public void getEconomyBasenewsnews(){
-        getBasenews("경제");
-    }
+    // @Scheduled(cron = "0 35 00 ? * *")
+    // public void getEconomyBasenewsnews(){
+    //     getBasenews("경제");
+    // }
 
 //    @Scheduled(cron = "0 15 12 ? * *")
 //    public void getSocietyBasenewsnews(){
@@ -77,6 +77,19 @@ public class GetNaverNews {
 //    public void getPoliticsBasenewsnews(){
 //        getBasenews("정치");
 //    }
+
+    @Scheduled(cron = "0 33 01 ? * *")
+    @Transactional
+    public void test() {
+
+        Optional<Keyword> keyword = keywordRepository.findByName("학비");
+
+        //타이틀, 원문,아읻
+        keywordProcessingService.processKeyword(keyword.get().getName(),keyword.get(),false,3);
+        entityManager.flush();
+        
+
+    }
 
 
 
